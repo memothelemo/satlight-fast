0c0c5c1fa9e051a208669415aa66eb30771d3aa1
memothelemo <memothelemo@protonmail.com>
2022-07-03 21:57:05 +0800

    Add fuzz testing
    Because the parser uses unsafe stuff, we need to strictly
    test its reliability to avoid unexpected crashes among our
    users.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
+#[cfg(feature = "ast")]
 pub extern crate satlight_ast as ast;
+
+#[cfg(feature = "common")]
 pub extern crate satlight_common as common;
+
+#[cfg(feature = "parser")]
+pub extern crate satlight_parser as parser;
